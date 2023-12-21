b72eb943b08eae4f0186cce700e385206b8cc08d
daffaYuharshu <daffayuharshu@gmail.com>
2023-12-21 22:38:47 +0700

    add README.md

--- aiang_db.sql
+++ aiang_db.sql
@@ -17,9 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `aiang_db`
---
+-- Create the database
+CREATE DATABASE IF NOT EXISTS `aiang_db`;
+USE `aiang_db`;
 
 -- --------------------------------------------------------
 
